a2427b4af79e332e374d1222567edf6d5bfb0167
calvinrzachman <calvin.zachman@protonmail.com>
2018-12-01 14:15:53 -0600

    updated comments

--- src/components/Message.js
+++ src/components/Message.js
@@ -1,10 +1,7 @@
 import React from "react";
 
 const message = props => {
-  // console.log("Props: ", props);
   const isUser = props.isUser ? "user" : "watson";
-  // console.log("User?: ", isUser);
-  // console.log("Message: ", props.message);
   return (
     <section className={"messageContainer"}>
       <article className={"message " + isUser}>{props.message}</article>
