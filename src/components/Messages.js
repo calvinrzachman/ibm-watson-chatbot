d61d44e2d24d5ee446620b7b6648f1bce197a39a
calvinrzachman <calvin.zachman@protonmail.com>
2018-12-01 08:50:43 -0600

    updated comments on Messages

--- src/components/Messages.js
+++ src/components/Messages.js
@@ -5,6 +5,7 @@ import Message from "./Message";
 class Messages extends Component {
   renderConversation() {
     // This currently rerenders the whole conversation as opposed to just appending a new message
+    // Updating DOM with new message was not an issue in Vanilla JS version of app
     return this.props.conversation.map((msgObject, index) => {
       return (
         <Message
